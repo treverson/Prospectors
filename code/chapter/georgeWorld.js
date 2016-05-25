e5753adc7e1277b44356c43703bbbecd3c3f8f55
George Witwer <gdavid.witwer@gmail.com>
2016-05-24 20:34:06 -0400

    need to fit in runAnimation

--- code/chapter/georgeWorld.js
+++ code/chapter/georgeWorld.js
@@ -26,7 +26,15 @@ Design:
 		- Can purchase "reinforcements" to support the mine shaft and prevent cave-ins
 	- Clicking "moves" the player forward, like he is creating a tunnel. This is, clearing a path.
 		- Blocks don't fill in but player can choose to move forward.
+
+Implementation notes:
+	- Initialize:
+		- Background
+		- Initial actor state
+		- Event listeners
+	- Step takes current time and user input and adjust actors accordingly.
 */
+
 
 /*
 	World interface
@@ -36,31 +44,97 @@ var World = function() {
 	// Idea: create an array of functions that need to be created?
 
 	// Return initial DOM element to attach OR -- Do stepping...?
-	this.init = function() { /* Requires implementation */ };
+	this.init = function(display, rootEl) { /* Requires implementation */ };
 	// A step in the function with time = t
-	this.step = function(t) { /* Requires implementation */ };
+	this.step = function(time, input) { /* Requires implementation */ };
 	// The background layer to draw once
 	this.backgroundLayer = function() { /* Requires implementation */ };
 	// The actors to draw each iteration
-	this.createActors = function { /* Requires implementation */};
+	this.createActors = function() { /* Requires implementation */ };
 	this.actors = [];
 };
 
 var Prospectors = function() {};
 Prospectors.prototype = Object.create(World);
-Prospectors.init = function() {
+Prospectors.prototype.init = function(display, rootEl) {
+
+	var runAnimation = function(frameFunc) {
+		var lastTime = null;
+		function frame(time) {
+			var stop = false;
+			if (lastTime != null) {
+				var timeStep = Math.min(time - lastTime, 100) / 1000;
+				stop = frameFunc(timeStep) === false;
+			}
+			lastTime = time;
+			if (!stop)
+				requestAnimationFrame(frame);
+		}
+		requestAnimationFrame(frame);
+	};
+
+	this.createActors();
+	this.drawBackground();
+	this.drawActors();
+	var keys = this.createEventListeners();
+	var start = Date.now();
+
+	while (Date.now() - start < 1000) {
+		this.step(Date.now(), keys);
+	}
+	console.log(this.actors);
+};
+
+/*
+	step
+	- Takes current time and user input,
+	- Modifies actors based on these variables.
+*/
+Prospectors.prototype.step = function(time, input) {
+	for (k in input) {
+		if (input[k]) {
+			console.log(k);
+		}
+	}
+};
+
+Prospectors.prototype.drawActors = function() {
 
 };
 
-Prospectors.step = function() {
+Prospectors.prototype.drawBackground = function() {
 
 };
 
-Prospectors.backgroundLayer = function() {
+Prospectors.prototype.createEventListeners = function() {
+	var arrowCodes = { 37: 'left', 38: 'up', 39: 'right', 40: 'down' };
+
+	function trackKeys(codes) {
+		var pressed = Object.create(null);
+		function handler(event) {
+			if (codes.hasOwnProperty(event.keyCode)) {
+				var down = event.type == 'keydown';
+				pressed[codes[event.keyCode]] = down;
+				event.preventDefault();
+			}
+		}
+		addEventListener('keydown', handler);
+		addEventListener('keyup', handler);
+		return pressed;
+	}
+
+	return trackKeys(arrowCodes);
 
 };
 
-Prospectors.actors = [];
+Prospectors.prototype.backgroundLayer = function() {
+
+};
+
+Prospectors.prototype.createActors = function() {
+	console.log('create actors');
+	this.actors = [];
+};
 
 var Display = function() {
 
@@ -70,16 +144,14 @@ var Display = function() {
 		if (className) {
 			elt.className = className;
 		}
-		return elt;
+		return elt;	
 	};
 };
 
 var main = function(world) {
 	var state = world; // hmm.. let's worry about this later.
 	var display = new Display();
-	var start = Date.now();
-	var initDOM = 'main';
-
-	// Step until done...
+	var rootEl = 'main';
+	world.init(display, rootEl);
 
 };
